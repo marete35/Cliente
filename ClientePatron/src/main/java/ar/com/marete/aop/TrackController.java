6186c889ba6003998fb0f3a9ce2a80cf568f9abf
Mario <mgalarza@learsoft.com.ar>
2020-02-05 10:36:51 -0300

    Se agrego para loguear la url.

--- ClientePatron/src/main/java/ar/com/marete/aop/TrackController.java
+++ ClientePatron/src/main/java/ar/com/marete/aop/TrackController.java
@@ -1,13 +1,14 @@
 package ar.com.marete.aop;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import ar.com.marete.base.controllers.BaseController;
 
@@ -16,6 +17,8 @@ import ar.com.marete.base.controllers.BaseController;
 public class TrackController {
 	
 	private static Logger logger = LoggerFactory.getLogger(BaseController.class);
+	@Autowired(required = false)
+	private HttpServletRequest request;
 
 	
 	/*@After ("args(name)")
@@ -25,31 +28,12 @@ public class TrackController {
 	}*/
 	
 	@Before ("execution (public * ar.com.marete.base.controllers.*.*())")
-	public void adviceControllersEndPoint(JoinPoint jp,RequestMapping request){
-		//logger.debug ("Executing Advice on getName()"+jp.getSignature().getName ());
-		/*MethodSignature signature = (MethodSignature) jp.getSignature();
-		String methodName = signature.getMethod().getName();
-		Class<?>[] parameterTypes = signature.getMethod().getParameterTypes();
-		Annotation[][] annotations = jp.getTarget().getClass().getMethod(methodName,parameterTypes).getParameterAnnotations();*/
+	public void adviceControllersEndPoint(JoinPoint jp){
+		System.out.println(request.getRequestURL());
+		System.out.println(request.getServletPath());
+		logger.debug ("Url: "+request.getServletPath()+" Clase: "+jp.getTarget().getClass().getSimpleName()+" Metodo: "+jp.getSignature().getName ());
 
-		System.out.println(request.path());
-		logger.debug ("Clase: "+jp.getTarget().getClass().getSimpleName()+" Metodo: "+jp.getSignature().getName ());
-		
-		
-
-		@Pointcut(
-		        "within(@org.springframework.stereotype.Controller *) && " +
-		        "@annotation(requestMapping) && " +
-		        "execution(* *(..))"
-		    )
-		    public void controller(RequestMapping requestMapping) {}
-	
-	 @Before("controller(requestMapping)")
-	    public void advice(JoinPoint thisJoinPoint, RequestMapping requestMapping) {
-	        System.out.println(thisJoinPoint);
-	        System.out.println("  " + requestMapping);
-	        System.out.println("  " + requestMapping.method()[0]);
-	    }
+    }
 	
 
 }
